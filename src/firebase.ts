1baff24b00f68366ad9134738239508d1353e37e
Nikolay Tverdokhlebov <nn.tverd@gmail.com>
2025-01-19 23:54:56 +0500

    fix: update condition

--- src/firebase.ts
+++ src/firebase.ts
@@ -15,13 +15,15 @@ export const removePublished = async () => {
     for (const document of documents) {
         console.log(document);
         const documentRef = doc(collectionRef, document.id);
-        if (document.mediaContainerId && document.status !== 'published') {
+        if (document.mediaContainerId && document.status === 'published') {
             const firebaseUrl = document.firebaseUrl;
-            const filePath = decodeURIComponent(firebaseUrl.split('/o/')[1].split('?')[0]);
+            if (firebaseUrl) {
+                const filePath = decodeURIComponent(firebaseUrl.split('/o/')[1].split('?')[0]);
 
-            console.log(filePath); // Выведет: 0IG9JusjhjTRWX8Yd1G9.mp4
-            const fileRef = ref(storage, filePath);
-            await deleteObject(fileRef);
+                console.log(filePath); // Выведет: 0IG9JusjhjTRWX8Yd1G9.mp4
+                const fileRef = ref(storage, filePath);
+                await deleteObject(fileRef);
+            }
 
             await deleteDoc(documentRef);
             break;
