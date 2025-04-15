d66291586eab32d13b30d1e3cf4aeb0dfb0b9600
Nikolay Tverdokhlebov <nn.tverd@gmail.com>
2025-04-15 17:09:01 +0500

    refactor: rename req parameter to _req in pingPong function for clarity

--- src/controllers/ping.ts
+++ src/controllers/ping.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from 'express';
 
-export const pingPong = (req: Request, res: Response): void => {
+export const pingPong = (_req: Request, res: Response): void => {
     res.status(200).json({
         status: 'success',
         message: 'pong',
