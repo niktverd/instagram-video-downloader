60d2f9399392ecbe2ae205992789a5b2da060512
Nikolay Tverdokhlebov <nn.tverd@gmail.com>
2025-06-04 02:47:24 +0500

    fix: update BaseEntitySchema to allow string or date for createdAt and updatedAt
    - Modified createdAt and updatedAt fields in BaseEntitySchema to accept both string and date types, enhancing flexibility in data handling.

--- src/schemas/models/base.ts
+++ src/schemas/models/base.ts
@@ -2,8 +2,8 @@ import {z} from 'zod';
 
 export const BaseEntitySchema = z.object({
     id: z.number(),
-    createdAt: z.date().optional(),
-    updatedAt: z.date().optional(),
+    createdAt: z.string().or(z.date()).optional(),
+    updatedAt: z.string().or(z.date()).optional(),
 });
 
 export const createEntitySchema = <T extends z.ZodRawShape>(shape: T) =>
